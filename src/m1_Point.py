b312e3c399a1568f8a77ae23c211dc43f44d56cf
hariv1 <hariv@rose-hulman.edu>
2017-12-18 16:17:43 -0500

    DONE

--- src/m1_Point.py
+++ src/m1_Point.py
@@ -95,6 +95,19 @@ class Point(object):
     def get_distance_traveled(self):
         return self.change
 
+    def closer_to(self,p2,p3):
+        distance_p2 = self.get_distance_from(p2)
+        distance_p3 = self.get_distance_from(p3)
+
+        if distance_p2 < distance_p3:
+            return p2
+        elif distance_p3 < distance_p2:
+            return p3
+        else:
+            return p2
+
+
+
 
 
 
@@ -1004,6 +1017,34 @@ def run_test_closer_to():
     print('-----------------------------------------------------------')
     print('Testing the   closer_to   method of the Point class.')
     print('-----------------------------------------------------------')
+
+    p1 = Point(10, 20)
+    p2 = Point(15, 20)
+    p3 = Point(14, 24)
+
+    print()
+    print('Expected:', p2)
+    print('Actual:  ', p1.closer_to(p2, p3))
+    print('Expected:', p2)
+    print('Actual:  ', p1.closer_to(p3, p2))
+
+    print()
+    print('Expected:', p1)
+    print('Actual:  ', p1.closer_to(p1, p3))
+    print('Expected:', p2)
+    print('Actual:  ', p2.closer_to(p3, p2))
+    print('Expected:', p3)
+    print('Actual:  ', p3.closer_to(p3, p3))
+
+    print()
+    p4 = p1.clone()
+    p5 = p1.clone()
+    print('Expected:', p4)
+    print('Actual:  ', p1.closer_to(p4, p5))
+    print('Expected: True')
+    print('Actual:  ', p1.closer_to(p4, p5) is p4)
+    print('Expected: False')
+    print('Actual:  ', p1.closer_to(p4, p5) is p5)
 
 
 def run_test_halfway_to():
